8926819555adf57113aa798639a3efbbed2825dd
9Jawad <chersam1970@gmail.com>
2025-05-11 19:50:11 +0200

    adding a GlobalModel (1)

---
+++ src/main/java/be/ulb/stib/data/GlobalModel.java
@@ -0,0 +1,25 @@
+package be.ulb.stib.data;
+
+import it.unimi.dsi.fastutil.ints.IntArrayList;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+
+
+/* Données fusionnées de toutes les agences, plus index spatial. */
+public final class GlobalModel extends AgencyModel {
+
+    public final ObjectArrayList<IntArrayList> walkNeighbors = new ObjectArrayList<>();
+    public final ObjectArrayList<IntArrayList> transitEdges = new ObjectArrayList<>();
+
+    public GlobalModel() {
+        super();
+        initSpatial();
+    }
+
+    private void initSpatial() {
+        int nStops = stopCount();
+        for (int i = 0; i < nStops; i++) {
+            walkNeighbors.add(new IntArrayList());
+            transitEdges.add(new IntArrayList());
+        }
+    }
+}
