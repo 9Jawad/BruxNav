5e98cc6db73dd2ce6960580ec54c58391f0a23f6
9Jawad <chersam1970@gmail.com>
2025-05-15 02:25:53 +0200

    MultiModalGraphTest

--- src/main/java/be/ulb/stib/graph/MultiModalGraph.java
+++ src/main/java/be/ulb/stib/graph/MultiModalGraph.java
@@ -25,28 +25,29 @@ public final class MultiModalGraph {
         ensureSize(this.costs,   nStops, null);
 
         for (int stopIdx = 0; stopIdx < nStops; stopIdx++) {
+            if (model.latList.get(stopIdx) < 0) continue;
 
-            IntArrayList target_transit = new IntArrayList();
-            IntArrayList target_cost = new IntArrayList();
+            IntArrayList targetTransit = new IntArrayList();
+            IntArrayList targetCost = new IntArrayList();
 
             // Arcs Marche
             IntArrayList walkT = model.walkEdges.get(stopIdx);
             IntArrayList walkC = model.walkEdgesCost.get(stopIdx);
             if (walkT != null) {
-                target_transit.addAll(walkT);
-                target_cost.addAll(walkC);
+                targetTransit.addAll(walkT);
+                targetCost.addAll(walkC);
             }
             // Arcs Transit
             IntArrayList trT = model.transitEdges.get(stopIdx);
             IntArrayList trC = model.transitEdgesCost.get(stopIdx);
             if (trT != null) {
-                target_transit.addAll(trT);
-                target_cost.addAll(trC);
+                targetTransit.addAll(trT);
+                targetCost.addAll(trC);
             }
             // ajout dans le graphe
-            if (!target_transit.isEmpty()) {
-                targets.set(stopIdx, target_transit);
-                targets.set(stopIdx, target_cost);
+            if (!targetTransit.isEmpty()) {
+                targets.set(stopIdx, targetTransit);
+                costs.set(stopIdx, targetCost);
             }
         }
     }
